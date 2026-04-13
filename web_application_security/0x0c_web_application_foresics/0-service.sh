137cba63c8e2e87789e7f90020b0f48e7cd78179
Nihad337 <nihadhmdov685@gmail.com>
2026-04-13 14:27:11 +0400

    test

--- web_application_security/0x0c_web_application_foresics/0-service.sh
+++ web_application_security/0x0c_web_application_foresics/0-service.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# Description: Script to identify the service used in web application attacks
-# Usage: ./0-service.sh auth.log
-
-grep pam_unix $1 | awk '{print $6, $7}' | sort | uniq -c | sort -nr
+grep -oP 'pam_unix\(\K[^:]+' auth.log | sort | uniq -c | sort -nr
